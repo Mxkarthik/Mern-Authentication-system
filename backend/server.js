9c60a15959d46c756c24aaf6f2c19e6dfa8821e9
Karthik Medapalli <maxwhitie1234@gmail.com>
2025-09-09 21:12:49 +0530

    server data

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,10 @@
 //const express = require('express');
 import express from 'express';
+
+const app = express();
+
+
+
+app.listen(3000, ()=> {
+    console.log("server is running on port 3000");
+});
